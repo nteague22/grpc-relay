0484f5a9f88cf1161789f9e3df3e9a218b8d2c89
Nathan Teague <nteague22@outlook.com>
2019-12-12 23:47:09 -0500

    need to set up SSL to test

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Google.Protobuf;
+using Grpc.Core;
+using Grpc.Net.Client;
+using MicroService.Protos;
 
 namespace ConsoleClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new MicroService.Protos.Manager.ManagerClient(GrpcChannel.ForAddress("https://localhost:5001"));
+
+            var newEntry = await client.AddAsync(new NewCustomer()
+            {
+                Email = "sample@email.com",
+                FirstName = "Joe",
+                LastName = "Customer"
+            });
+
+            Console.WriteLine($"The new customer number is {newEntry.Id}");
+            Console.ReadKey();
         }
     }
 }
---
+++ MicroService/Services/CustomerManagerService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using MicroService.Protos;
+using Microsoft.Extensions.Logging;
+
+namespace MicroService.Services
+{
+    public class CustomerManagerService : Manager.ManagerBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+
+        public CustomerManagerService(ILogger<GreeterService> logger)
+        {
+            this._logger = logger;
+        }
+
+        public async override Task<CustomerEntry> Add(NewCustomer request, ServerCallContext context)
+        {
+            return await Task.FromResult(new CustomerEntry()
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                DateAdded = Timestamp.FromDateTime(DateTime.UtcNow),
+                Id = Guid.NewGuid().ToString()
+            });
+        }
+
+        public async override Task<CustomerDeleted> Delete(DeleteCustomer request, ServerCallContext context)
+        {
+            return await Task.FromResult(new CustomerDeleted() { Id = request.Id, DateDeleted = Timestamp.FromDateTime(DateTime.UtcNow) });
+        }
+    }
+}
